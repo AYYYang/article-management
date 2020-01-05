758c913b11a38c225b17ebceefb802d28f4e6773
AmandaYang <yuanyuan.yang@alumni.ubc.ca>
2020-01-05 11:21:13 +0800

    Added register functionality

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,19 @@ router.get('/', function(req, res, next) {
   model.connect(function (db) {
     db.collection('users').find().toArray(function(error, docs) {
       console.log('user list:', docs);
-      res.render('index', { title: 'Express' });
+      res.render('index', { title: 'Homepage' });
     });
   });
+});
+
+/* GET Register page. */
+router.get('/register', function (req, res, next) {
+  res.render('register', {});
+});
+
+/* GET Login page. */
+router.get('/login', function (req, res, next) {
+  res.render('login', {});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var model = require('../model');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+/* POST users to send user data to /register */
+router.post('/register', function(req,res,next) {
+  const user = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    password2: req.body.password2
+  };
+
+  model.connect(function (db) {
+    db.collection('users').insertOne(user, function (error, ret) {
+      if (error) {
+        console.error('failed to register');
+        // redirect to register page
+        res.redirect('/register');
+      } else {
+        res.redirect('/login')
+      }
+    })
+  });
 });
 
 module.exports = router;
